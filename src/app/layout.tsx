e8f3965280bda7043ffe747c11272b2671af7b3c
jus1d <artfa63@gmail.com>
2024-11-29 18:55:42 +0400

    cleanup

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,22 +1,18 @@
-// app/layout.tsx
 import { Notification } from "@/components/notification";
 import type { Metadata } from "next";
-import { cookies } from 'next/headers'; // Import cookies utility
+import { cookies } from 'next/headers';
 import "./globals.css";
 import { Header } from "@/components/header";
 import { TonConnectProvider } from "@/components/ton-connect/provider";
 import { Footer } from "@/components/footer";
 import AlertTriangle from "@/icons/alert-triangle";
 
-// Define the metadata for the page
 export const metadata: Metadata = {
   title: "Cascade",
   description: "Host, compete & win",
 };
 
-// Define the RootLayout component
 export default async function RootLayout({ children }: Readonly<{ children: React.ReactNode }>) {
-  // Access cookies
   const cookieStore = cookies();
   const theme: 'light' | 'dark' = cookieStore.get('theme')?.value === 'dark' ? 'dark' : 'light'; // Default to 'light' if no cookie is set
 
